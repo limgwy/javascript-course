65f169847be44bc116666384b54de74a603bd13b
Limgwy <limgwyenth@gmail.com>
2025-09-20 13:57:23 +0800

    feat(modal): add keyboard accessibility, focus management, and ARIA attributes

--- 05-modal/starter/script.js
+++ 05-modal/starter/script.js
@@ -12,24 +12,41 @@ const btnCloseModalEl = document.querySelector('.close-modal');
 //Open Modal
 const btnOpenModalEl = document.querySelectorAll('.show-modal');
 
+let lastFocusedButton = null;
 
 //Open modal function
 const openModal = function() {
     modalEl.classList.remove('hidden');
 
     overlayEl.classList.remove('hidden');
-}
 
-btnOpenModalEl.forEach(btn => btn.addEventListener('click', openModal))
+    modalEl.focus();
+    lastFocusedButton = document.activeElement;
+}
 
 //Close Modal 
-
 const closeModal = function() {
     modalEl.classList.add('hidden');
-
     overlayEl.classList.add('hidden');
-}
+
+    if (lastFocusedButton) {
+        lastFocusedButton.focus();
+    }
+};
 
 btnOpenModalEl.forEach(btn => btn.addEventListener('click', openModal))
 btnCloseModalEl.addEventListener('click', closeModal);
 overlayEl.addEventListener('click', closeModal);
+
+// close modal with key
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modalEl.classList.contains('hidden')){
+        closeModal();
+    }
+
+});
+
+modalEl.setAttribute('role', 'dialog');
+modalEl.setAttribute('aria-modal', 'true');
+
+btnCloseModalEl.setAttribute('aria-label', 'Close-modal');
